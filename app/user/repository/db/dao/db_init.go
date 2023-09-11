db736c55a1150ea1c1ca3afd9171688cbed1a7fe
xzlyqs <xzlyqs@163.com>
2023-09-11 10:32:56 +0800

    commit

--- app/user/repository/db/dao/db_init.go
+++ app/user/repository/db/dao/db_init.go
@@ -1,11 +1,16 @@
 package dao
 
 import (
+	"context"
 	"fmt"
-	"github.com/jinzhu/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 	"micro-todoList/micro-todoList/config"
 	"strings"
+	"time"
 )
 
 var _db *gorm.DB
@@ -26,5 +31,39 @@ func InitDB() {
 
 func Database(connString string) error {
 	var ormLogger logger.Interface
+	if gin.Mode() == "debug" {
+		ormLogger = logger.Default.LogMode(logger.Info)
 
+	} else {
+		ormLogger = logger.Default
+	}
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       connString,
+		DefaultStringSize:         256,
+		DisableDatetimePrecision:  true,
+		DontSupportRenameIndex:    true,
+		DontSupportRenameColumn:   true,
+		SkipInitializeWithVersion: false,
+	}), &gorm.Config{
+		Logger: ormLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Second * 30)
+	_db = db
+	migration()
+	return err
+
+}
+
+func NewDBClient(ctx context.Context) *gorm.DB {
+	db := _db
+	return db.WithContext(ctx)
 }
--- app/user/repository/db/dao/user.go
+++ app/user/repository/db/dao/user.go
@@ -1,1 +1,30 @@
 package dao
+
+import (
+	"context"
+	"github.com/CocaineCong/micro-todoList/app/user/repository/db/model"
+	"gorm.io/gorm"
+)
+
+type UserDao struct {
+	*gorm.DB
+}
+
+func NewUserDap(ctx context.Context) *UserDao {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	return &UserDao{NewDBClient(ctx)}
+}
+
+func (dao *UserDao) FindUserByUserName(userName string) (r *model.User, err error) {
+	err = dao.Model(&model.User{}).Where("user_name=?", userName).Find(&r).Error
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (dao *UserDao) CreateUser(in *model.User) (err error) {
+	return dao.Model(&model.User{}).Create(&in).Error
+}
